691b3304353405577cbcb35f618521920f073d0d
ampersand-github <ampersand.dev@gmail.com>
2023-07-13 17:48:03 +0900

    feat: 環境構築中

--- src/app/(root)/index.stories.tsx
+++ src/app/(root)/index.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 // todo 最低一個のstoryを書く必要があるからかいた。後で消す
 const Button = () => {
   return <button>button</button>;
-}
+};
 
 const meta: Meta<typeof Button> = {
   title: "components/sampleStory",
